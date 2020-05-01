5ed2dc5d51cfe45af0a70061d3866512bf841f2e
rmautia <raphaelnyangenya@gmail.com>
2020-05-02 01:39:29 +0300

    add.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
-from .main import main
+#from .main import main
 from flask import Blueprint
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
+
 
 def create_app(config_name):
 
@@ -12,9 +20,12 @@ def create_app(config_name):
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    #config_options[config_name].init_app(app)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
      # Registering the blueprint
     from .main import main as main_blueprint
@@ -24,4 +35,9 @@ def create_app(config_name):
     from .requests import configure_request
     configure_request(app)
 
+     ###
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
     return app
+
